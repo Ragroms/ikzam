1de88aa6a4fe266abd43458a879214d5ec9fa3de
Ragroms <127696223+Ragroms@users.noreply.github.com>
2023-06-07 19:14:49 +0300

    Add files via upload

---
+++ ПР11/ПР11.Задание1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Пр11
+{
+    internal class Program // Определить и вывести: количество предложений и слов каждого предложения; тексткаждого предложения в отдельной строке. Даны 2 слова. Определить, есть ли они в тексте и сколько раз каждое из них встречается в тексте.Если их в тексте нет, дать соответствующее сообщение.
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать!♥");
+            string filePath = "ПР11 блакнот.txt";
+
+            // Считываем текст из файла
+            string text = File.ReadAllText(filePath);
+
+            // Заданные слова
+            Console.WriteLine("Ввудите первое слово: ");
+            string word1 = Console.ReadLine();
+            Console.WriteLine("Ввудите второе слово: ");
+            string word2 = Console.ReadLine();
+
+            // Поиск и подсчет вхождений слов в текст
+            int resultWord1 = text.Split(' ','.',',','!',';','♥','☻','/',':','?').Count(w => w == word1);
+            int resultWord2 = text.Split(' ','.',',','!',';','♥','☻','/',':','?').Count(w => w == word2);
+
+            // Вывод результата
+            if (resultWord1 == 0 && resultWord2 == 0)
+            {
+                Console.WriteLine($"Слова '{word1}' и '{word2}' не найдены в тексте.");
+            }
+            else
+            {
+                Console.WriteLine($"Слово '{word1}' встречается в тексте {resultWord1} раз(а).");
+                Console.WriteLine($"Слово '{word2}' встречается в тексте {resultWord2} раз(а).");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ПР11/ПР11_Задание2/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace ПР11_Задание2
+{
+    internal class Program // Разработать консольное приложение для обработки матриц в соответствии с вариантом задания.В каждом варианте задания исходным является файл с элементами матрицы.Необходимосчитать исходные данные, вывести их на экран для просмотра и выполнить обработку. Результат обработки вывести на экран и в файл. Задан массив X(N). Расположить элементы в нем в обратном порядке. Найти сумму элементов с нечетными индексами.
+
+    {
+            static void Main()
+            {
+            Console.WriteLine("Добро пожаловать!♥");
+            int[] x;
+
+            // Читаем данные из файла
+            using (StreamReader sr = new StreamReader("Массив11.1.txt"))
+            {
+                string line = sr.ReadLine();
+                string[] numbers = line.Split(' ');
+                x = new int[numbers.Length];
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    x[i] = int.Parse(numbers[i]);
+                }
+            }
+
+            // Выводим исходный массив
+            Console.WriteLine("Исходный массив:");
+            foreach (int num in x)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+
+            // Реверс массива
+            Array.Reverse(x);
+
+            // Выводим результат
+            Console.WriteLine("Результат:");
+            foreach (int num in x)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+
+            // Находим сумму элементов с нечетными индексами
+            int sum = 0;
+            for (int i = 1; i < x.Length; i += 2)
+            {
+                sum += x[i];
+            }
+
+            // Выводим результат на экран и в файл
+            Console.WriteLine("Сумма элементов с нечетными индексами: " + sum);
+            using (StreamWriter sw = new StreamWriter("Массив11.2.txt"))
+            {
+                sw.WriteLine("Сумма элементов с нечетными индексами: " + sum);
+            }
+
+            Console.ReadKey();
+            }
+        }
+    }
