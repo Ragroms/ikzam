d51f09b6882c8e2429939f7d7dc2a8ceefa845d0
Ragroms <127696223+Ragroms@users.noreply.github.com>
2023-06-07 22:28:10 +0300

    Add files via upload

---
+++ Задание 5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Задание_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку S:");
+            string S = Console.ReadLine();
+            Console.WriteLine("Введите строку S1:");
+            string S1 = Console.ReadLine();
+            Console.WriteLine("Введите строку S2:");
+            string S2 = Console.ReadLine();
+
+            string result = S.Replace(S1, S2);
+            Console.WriteLine("Результат замены: " + result);
+        }
+    }
+}
